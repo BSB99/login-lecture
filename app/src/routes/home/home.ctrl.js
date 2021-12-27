e6db61b97937dd8e700f596ae66e1c8a0f713f55
Seung Bum <wer06099@naver.com>
2021-12-27 11:59:30 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+  id: ["가가", "나나", "다다"],
+  psword: ["1234", "1234", "123456"],
+};
+
 const output = {
   home: (req, res) => {
     res.render("home/index");
@@ -12,7 +17,22 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   },
 };
 
